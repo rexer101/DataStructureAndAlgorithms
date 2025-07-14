f403e6de3eef3b5f368b4ec432f7657f84a230ea
Ravi kushwaha <ravikushwaha18@hotmail.com>
2025-07-14 17:11:48 +0200

    Added last stone weight ii

---
+++ DSA/DP/Minimum (Maximum) Path to Reach a Target/LastStoneWeightII.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.DP.Minimum__Maximum__Path_to_Reach_a_Target
+{
+    internal class LastStoneWeightII
+    {
+        public void start()
+        {
+            Console.WriteLine(BottomUp(12));
+            Console.WriteLine(TopDown(10));
+        }
+
+        private int BottomUp(int n)
+        {
+            return 0;
+        }
+
+        private int TopDown(int n)
+        {
+            return 0;
+        }
+    }
+}
