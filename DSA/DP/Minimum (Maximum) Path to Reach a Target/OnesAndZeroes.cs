b83a7601f9b583f8cd8ebd2abd9549ceb85bc357
Ravi kushwaha <ravikushwaha18@hotmail.com>
2025-07-20 19:11:52 +0200

    Added solution of ones and zeroes

--- DSA/DP/Minimum (Maximum) Path to Reach a Target/OnesAndZeroes.cs
+++ DSA/DP/Minimum (Maximum) Path to Reach a Target/OnesAndZeroes.cs
@@ -10,13 +10,43 @@ namespace DSA.DP.Minimum__Maximum__Path_to_Reach_a_Target
     {
         public void start()
         {            
-            Console.WriteLine(BottomUp());
+            Console.WriteLine(BottomUp(["10", "0001", "111001", "1", "0"], 5, 3));
             Console.WriteLine(TopDown());
         }
 
-        private int BottomUp()
+        private int BottomUp(string[] strs, int m, int n)
         {
-            return 0;
+            int[][] dp = new int[m + 1][];
+
+            for (int i = 0; i <= m; i++) 
+            {
+                dp[i] = new int[n + 1];
+            }
+
+            foreach(string input in strs)
+            {
+                int ones = 0;
+                int zeroes = 0;
+
+                foreach (char str in input)
+                {
+                    if(str == '1') ones++;
+                    else zeroes++;
+                }
+
+
+                for (int i = m; i >= zeroes; i--)
+                {
+                    for (int j = n; j >= ones; j--)
+                    {
+                        int value = Math.Max(dp[i][j], dp[i - zeroes][j - ones] + 1);
+                        dp[i][j] = value;
+                    }
+                }
+            }
+
+
+            return dp[m][n];
         }
 
         private int TopDown()
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,4 @@
 ﻿using DSA.DP.Minimum__Maximum__Path_to_Reach_a_Target;
 
-new Triangle().start();
+new OnesAndZeroes().start();
 Console.ReadLine();
