6bba3a234462459d2d48298165ae3d9a18e5e2c7
Ravi kushwaha <ravikushwaha18@hotmail.com>
2025-07-22 18:04:27 +0200

    added botom up for max square

--- DSA/DP/Minimum (Maximum) Path to Reach a Target/MaximalSquare.cs
+++ DSA/DP/Minimum (Maximum) Path to Reach a Target/MaximalSquare.cs
@@ -8,15 +8,42 @@ namespace DSA.DP.Minimum__Maximum__Path_to_Reach_a_Target
 {
     internal class MaximalSquare
     {
+        string[][] input = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]];
         public void start()
         {
-            Console.WriteLine(BottomUp());
+            Console.WriteLine(BottomUp(input));
             Console.WriteLine(TopDown());
         }
 
-        private int BottomUp()
+        private int BottomUp(string[][] input)
         {
-            return 0;
+            int n = input.Length;
+            if(n == 0) return 0;
+            int m = input[0].Length;
+            int max = 0;
+            int[][] dp = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = new int[m];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i][j] != '1') continue;
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i][j] = 1;
+                    }
+                    else
+                    {
+                        dp[i][j] = Math.Min(dp[i][j - 1], Math.Min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    }
+                    max = Math.Max(max, dp[i][j]);
+                }
+            }
+            return max;
         }
 
         private int TopDown()
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,4 +1,4 @@
 ﻿using DSA.DP.Minimum__Maximum__Path_to_Reach_a_Target;
 
-new OnesAndZeroes().start();
+new MaximalSquare().start();
 Console.ReadLine();
