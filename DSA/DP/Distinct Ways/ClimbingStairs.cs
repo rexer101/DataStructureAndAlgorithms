0b212182cc7e83c45da9c3e35171612ea0d632cc
Ravi kushwaha <ravikushwaha18@hotmail.com>
2025-07-26 20:33:53 +0200

    Added distinct ways

---
+++ DSA/DP/Distinct Ways/ClimbingStairs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.DP.Distinct_Ways
+{
+    internal class ClimbingStairs
+    {
+        public void start()
+        {
+            Console.WriteLine(BottomUp());
+            Console.WriteLine(TopDown());
+        }
+
+        private int BottomUp()
+        {
+            return 0;
+        }
+
+        private int TopDown()
+        {
+            return 0;
+        }
+    }
+}
